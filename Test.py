aede4f1e8da7a45a8884721dc2a84e8387010625
samyrd309 <samyrd309@gmail.com>
2022-06-15 19:37:50 -0400

    .

---
+++ Test.py
@@ -0,0 +1,11 @@
+import unittest
+from main import Programa
+
+class TestMainMethods(unittest.TestCase):
+    def test_interger_range_contains(self):
+         x = Programa.integer_range_contains(2)
+        
+    
+    
+if __name__ == '__main__':
+    unittest.main()
